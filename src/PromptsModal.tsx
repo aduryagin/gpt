62b2d44449a64cf335923a669c5b8192aa31435a
Alexey Duryagin <aiduryagih@gmail.com>
2023-03-16 19:02:45 +0400

    new layout for prompts

--- src/PromptsModal.tsx
+++ src/PromptsModal.tsx
@@ -7,13 +7,8 @@ interface Props {
   setMessage: (message: string) => void;
 }
 
-enum Tab {
-  English = 'English',
-  Custom = 'Custom',
-}
-
 export default function PromptsModal({ setMessage }: Props) {
-  const [tab, setTab] = useState<Tab>(Tab.English);
+  const [tab, setTab] = useState<string>(prompts[0].topic);
 
   return (
     <>
@@ -24,58 +19,55 @@ export default function PromptsModal({ setMessage }: Props) {
           <label htmlFor={modalId} className="btn btn-sm btn-circle absolute right-2 top-2">
             ✕
           </label>
-          <h3 className="font-bold text-lg">Prompts</h3>
-          <div className="py-4">
-            <div className="tabs mb-4">
-              {Object.values(Tab).map((tabName) => (
-                <a
-                  key={tabName}
-                  className={`tab tab-lg tab-lifted ${tab === tabName ? 'tab-active' : ''}`}
-                  href="/"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    setTab(tabName as Tab);
-                  }}
-                >
-                  {tabName}
-                </a>
-              ))}
+          <div>
+            <div className="grid grid-cols-8 gap-4">
+              <div className="col-span-2">
+                <ul className="menu bg-base-100 sticky top-0">
+                  <li className="menu-title">
+                    <span>English</span>
+                  </li>
+                  {prompts.map((group) => (
+                    <li key={group.topic}>
+                      <button
+                        className={`text-left ${tab === group.topic ? 'active' : ''}`}
+                        onClick={() => setTab(group.topic)}
+                      >
+                        {group.topic}
+                      </button>
+                    </li>
+                  ))}
+
+                  {/* <li className="menu-title">
+                    <span>Other</span>
+                  </li>
+                  <li>
+                    <a>Other</a>
+                  </li> */}
+                </ul>
+              </div>
+              <div className="col-span-6">
+                {prompts
+                  .find((item) => item.topic === tab)
+                  ?.list.map((prompt: any) => {
+                    const title = typeof prompt === 'string' ? '' : prompt.title;
+                    const text = typeof prompt === 'string' ? prompt : prompt.text;
+                    return (
+                      // eslint-disable-next-line react/jsx-key
+                      <label key={text} htmlFor={modalId}>
+                        {title && <b>{title}</b>}
+                        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+                        <div
+                          className="textarea textarea-bordered mb-2 hover:bg-slate-100"
+                          style={{ cursor: 'pointer' }}
+                          onClick={() => setMessage(text)}
+                        >
+                          {text}
+                        </div>
+                      </label>
+                    );
+                  })}
+              </div>
             </div>
-
-            {tab === Tab.English &&
-              prompts.map((group) => (
-                <div
-                  key={group.topic}
-                  // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-                  tabIndex={0}
-                  className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box mb-3"
-                >
-                  <input type="checkbox" defaultChecked />
-                  <div className="collapse-title text-xl font-medium">{group.topic}</div>
-                  <div className="collapse-content">
-                    {group.list.map((prompt) => {
-                      const title = typeof prompt === 'string' ? '' : prompt.title;
-                      const text = typeof prompt === 'string' ? prompt : prompt.text;
-                      return (
-                        // eslint-disable-next-line react/jsx-key
-                        <label key={text} htmlFor={modalId}>
-                          {title && <b>{title}</b>}
-                          {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
-                          <div
-                            className="textarea textarea-bordered mb-2 hover:bg-slate-100"
-                            style={{ cursor: 'pointer' }}
-                            onClick={() => setMessage(text)}
-                          >
-                            {text}
-                          </div>
-                        </label>
-                      );
-                    })}
-                  </div>
-                </div>
-              ))}
-
-            {tab === Tab.Custom && <div className="text-center">todo</div>}
           </div>
         </label>
       </label>
